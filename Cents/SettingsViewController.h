35dc3c5de4eb8e69f86d34508d389614dfccf5ea
Sapan Bhuta <sapanbhuta@gmail.com>
2014-08-21 00:03:21 -0400

    settings

---
+++ Cents/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  Cents
+//
+//  Created by Sapan Bhuta on 8/20/14.
+//  Copyright (c) 2014 SapanBhuta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+@end
